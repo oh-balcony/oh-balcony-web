2948fa13749a791c64a06ab1f6186a777d1be038
Hermann Czedik-Eysenberg <git-dev@hermann.czedik.net>
2016-12-05 17:46:36 +0100

    Add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ApiService } from './shared';
@@ -12,6 +14,7 @@ import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
 
 @NgModule({
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     HttpModule,
     FormsModule,
